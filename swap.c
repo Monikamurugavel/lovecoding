b1d03a2b936c934ac4802d11db54062fbc3c7eae
Monikamurugavel <92583916+Monikamurugavel@users.noreply.github.com>
2021-10-15 06:13:58 -0700

    updated swap.c
    this file  print  the swapping two numbers without using third  variables

--- swap.c
+++ swap.c
@@ -4,9 +4,9 @@ main()
     int a=10;
     int b=20;
     printf("\nbefore swapping two number\n a:%d b:%d",a,b);
-	a=a+b;
-	b=a-b;
-	a=a-b;
-	printf("\nafter swapping two number\n a:%d b:%d",a,b);
-	return 0;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("\nafter swapping two number\n a:%d b:%d",a,b);
+    return 0;
 }
